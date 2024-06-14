cb4a2c9811bce958e8485f7b23ea93e9b658d6f5
rahulray19 <100296364+rahulray19@users.noreply.github.com>
2024-06-14 18:35:29 +0530

    morpheus-50 client dataset

--- src/main/groovy/com/morpheusdata/commvault/datasets/CommvaultClientsDatasetProvider.groovy
+++ src/main/groovy/com/morpheusdata/commvault/datasets/CommvaultClientsDatasetProvider.groovy
@@ -2,9 +2,17 @@ package com.morpheusdata.commvault.datasets
 
 import com.morpheusdata.core.MorpheusContext
 import com.morpheusdata.core.Plugin
+import com.morpheusdata.core.data.DataAndFilter
+import com.morpheusdata.core.data.DataFilter
+import com.morpheusdata.core.data.DataOrFilter
+import com.morpheusdata.core.data.DataQuery
 import com.morpheusdata.core.data.DatasetInfo
 import com.morpheusdata.core.data.DatasetQuery
 import com.morpheusdata.core.providers.AbstractDatasetProvider
+import com.morpheusdata.model.Cloud
+import com.morpheusdata.model.Permission
+import com.morpheusdata.model.ReferenceData
+import com.morpheusdata.model.ResourcePermission
 import groovy.util.logging.Slf4j
 import io.reactivex.rxjava3.core.Observable
 
@@ -13,7 +21,7 @@ import io.reactivex.rxjava3.core.Observable
  */
 
 @Slf4j
-class CommvaultClientsDatasetProvider extends AbstractDatasetProvider<Map, Long> {
+class CommvaultClientsDatasetProvider extends AbstractDatasetProvider<ReferenceData, String> {
 
     public static final providerName = 'Commvault Clients Dataset Provider'
     public static final providerNamespace = 'commvault'
@@ -35,44 +43,169 @@ class CommvaultClientsDatasetProvider extends AbstractDatasetProvider<Map, Long>
         )
     }
 
-    Class<Map> getItemType() {
-        return Map.class
+    /**
+     * the class type of the data for this provider
+     * @return the class this provider operates on
+     */
+    @Override
+    Class<ReferenceData> getItemType() {
+        return null
     }
 
-    Observable<Map> list(DatasetQuery query) {
-        return Observable.fromIterable(members)
+    /**
+     * list the values in the dataset
+     * @param query the user and map of query params or options to apply to the list
+     * @return a list of objects
+     */
+    @Override
+    Observable<ReferenceData> list(DatasetQuery query) {
+        return null
     }
 
+    /**
+     * list the values in teh dataset in a common format of a name value pair. (example: [[name: "blue", value: 1]])
+     * @param query a DatasetQuery containing the user and map of query params or options to apply to the list
+     * @return a list of maps that have name value pairs of the items
+     */
+    @Override
     Observable<Map> listOptions(DatasetQuery query) {
-        return Observable.fromIterable(members)
-    }
+        log.debug("clients: ${query.parameters}")
+        List clients = []
+        Long cloudId = query.get("zoneId")?.toLong()
+        def account = query.user.account
+        Cloud cloud = null
+        def backupProvider
+        if(cloudId) {
+            cloud = morpheus.services.cloud.get(cloudId)
+        }
+        if (cloud && cloud.backupProvider) {
+            backupProvider = morpheus.services.backupProvider.find(new DataQuery().withFilter("account", account ).withFilter("id", cloud.backupProvider.id))
+        }
+        if (backupProvider) {
+            def accessibleResourceIds = morpheus.services.resourcePermission
+                    .listAccessibleResources(account.id, ResourcePermission
+                            .ResourceType.BackupServer, null, null) // dustin will add in enum class
+            //def accessibleResourcesQuery = ReferenceData.where { account == backupProvider.account && category == "${backupProvider.type.code}.backup.backupServer.${backupProvider.id}" && enabled == true }
+            def customDataQuery = new DataQuery().withFilters([
+                    new DataFilter("account", backupProvider.account),
+                    new DataFilter("category", "${backupProvider.type.code}.backup.backupServer.${backupProvider.id}"),
+                    new DataFilter("enabled", true)
+            ])
+            /*def accessibleResourcesQuery = morpheus.services.referenceData.list(new DataQuery()
+                    .withFilter()
+                    .withFilter()
+                    .withFilter())*/
+            if(accessibleResourceIds) {
+                customDataQuery.withFilters([
+                        new DataOrFilter("account", account),
+                        new DataFilter()
+                ])
+                def dataAndFilter = new DataAndFilter()
+                def dataOrFilter = new DataOrFilter()
+                dataAndFilter.withFilters("account.masterAccount", true).withFilters("visibility", "public")
+                dataOrFilter.withFilter("account", account)
+                customDataQuery = customDataQuery.withFilters([
 
-    Map fetchItem(Object value) {
-        def rtn = null
-        if (value instanceof Long) {
-            rtn = item((Long) value)
-        } else if (value instanceof CharSequence) {
-            def longValue = value.isNumber() ? value.toLong() : null
-            if (longValue) {
-                rtn = item(longValue)
+                ])
+                accessibleResourcesQuery = accessibleResourcesQuery.where{ (account == account || (account == masterAccount && visibility == 'public') || id in accessibleResourceIds) }
+            } else {
+                accessibleResourcesQuery = accessibleResourcesQuery.where{ (account == account || (account == masterAccount && visibility == 'public')) }
+            }
+            def clientResults = accessibleResourcesQuery
+            if (clientResults.size() > 0) {
+                clientResults.each { client ->
+                    def clientInstanceType = client.getConfigProperty('vsInstanceType')
+                    //def clientInstanceTypeCode = clientInstanceType ? commvaultBackupService.getvsInstanceType(clientInstanceType?.toString()) : null
+                    //getvsInstanceType // add this in utility class, create new utility class
+                    def clientInstanceTypeCode // check: ??
+                    if(!cloud || cloud?.cloudType?.provisionTypes.find { it.code == clientInstanceTypeCode }) {
+                        clients << [name: client.name, id: client.id, value: client.id]
+                    }
+                }
+            } else {
+                clients << [name: "No clients setup in Commvault", id:'']
             }
         }
-        return rtn
+
+
+
+        if(backupProvider) {
+            def accessibleResourceIds = permissionService.resourcesAccessibleByAccount(account.id, 'BackupServer') // permissionService?? // resouce permission serveice
+            def masterAccount = Account.findByMasterAccount(true) // ??
+            def accessibleResourcesQuery = ReferenceData.where { account == backupProvider.account && category == "${backupProvider.type.code}.backup.backupServer.${backupProvider.id}" && enabled == true }
+            // is dataquery for referenceData ??
+            if(accessibleResourceIds) {
+                accessibleResourcesQuery = accessibleResourcesQuery.where{ (account == account || (account == masterAccount && visibility == 'public') || id in accessibleResourceIds) }
+            } else {
+                accessibleResourcesQuery = accessibleResourcesQuery.where{ (account == account || (account == masterAccount && visibility == 'public')) }
+            }
+
+            def clientResults = accessibleResourcesQuery.list()
+            if (clientResults.size() > 0) {
+                clientResults.each { client ->
+                    def clientInstanceType = client.getConfigProperty('vsInstanceType')
+                    def clientInstanceTypeCode = clientInstanceType ? commvaultBackupService.getvsInstanceType(clientInstanceType?.toString()) : null
+                    if(!zone || zone?.zoneType?.provisionTypes.find { it.code == clientInstanceTypeCode }) {
+                        clients << [name: client.name, id: client.id, value: client.id]
+                    }
+                }
+            } else {
+                clients << [name: "No clients setup in Commvault", id:'']
+            }
+
+        } else {
+            clients << [name: "No Commvault backup provider found.", id:'']
+        }
+
+        return clients
     }
 
-    Map item(Long value) {
-        def rtn = members.find { it.value == value }
-        return rtn
+    /**
+     * returns the matching item from the list with the value as a string or object - since option values
+     *   are often stored or passed as strings or unknown types. lets the provider do its own conversions to call
+     *   item with the proper type. did object for flexibility but probably is usually a string
+     * @param value the value to match the item in the list
+     * @return the item
+     */
+    @Override
+    ReferenceData fetchItem(Object value) {
+        return null
     }
 
-    String itemName(Map item) {
-        return item.name
+    /**
+     * returns the item from the list with the matching value
+     * @param value the value to match the item in the list
+     * @return the
+     */
+    @Override
+    ReferenceData item(String value) {
+        return null
     }
 
-    Long itemValue(Map item) {
-        return (Long) item.value
+    /**
+     * gets the name for an item
+     * @param item an item
+     * @return the corresponding name for the name/value pair list
+     */
+    @Override
+    String itemName(ReferenceData item) {
+        return null
     }
 
+    /**
+     * gets the value for an item
+     * @param item an item
+     * @return the corresponding value for the name/value pair list
+     */
+    @Override
+    String itemValue(ReferenceData item) {
+        return null
+    }
+
+    /**
+     * Returns true if the Provider is a plugin. Always true for plugin but null or false for Morpheus internal providers.
+     * @return provider is plugin
+     */
     @Override
     boolean isPlugin() {
         return true
